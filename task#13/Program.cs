7373918c081607f32d728327b3ae1ed7aa5def1e
Arman_Dzhambulov <arman1407@ya.ru>
2023-05-25 06:40:46 +0300

    task#13

---
+++ task#13/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+return result;
+
+    if (number < 100);
+        {
+            number /= 10;
+            return number % 10;
+
+            else if (number > 999);
+            {
+                Console.WriteLine("Третьей цифры нет");
+                return false;
+            }
+        }   
+        Console.WriteLine(number);
+
+
+
+// Показать 2 цифру в любом трехзначном числе
+
+// Console.Clear();
+// int number = new Random().Next(100, 1000); // [100; 999]
+// Console.WriteLine(number);
+// Console.WriteLine(number / 100 * 10 + number % 100);
+
+
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine()!);
+// if (number < 100 || number >= 1000)
+// {
+//     Console.WriteLine("Вы ввели не то число. Повторите попытку");
+//     return;
+// }
+// Console.WriteLine(number / 10 % 10);
+
+
+
+
+
+
+
+
+
